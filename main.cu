0f1cfb3b62e34b80d72bf50cea5163c8bc9b7ce3
Hans-Henrik Fuxelius <hfuxelius@gmail.com>
2017-02-03 11:48:04 +0100

    CUDA is working

--- main.cu
+++ main.cu
@@ -179,104 +179,21 @@ int main(int argc, char *argv[]) {
 
     //point_square_GPU(&arc_table, arc_len, &mag_table, mag_len, arc_size);
 
-    point_square_GPU<<<grid, block>>>(&d_arc_table, arc_len, &d_mag_table, mag_len, arc_size);
+    point_square_GPU<<<grid, block>>>(d_arc_table, arc_len, d_mag_table, mag_len, arc_size);
 
     CHECK(cudaDeviceSynchronize());
 
     CHECK(cudaGetLastError());
 
-    //CHECK(cudaMemcpy(mag_table, d_mag_table, mag_bytes, cudaMemcpyDeviceToHost));
-    //CHECK(cudaMemcpy(arc_table, d_arc_table, arc_bytes, cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(mag_table, d_mag_table, mag_bytes, cudaMemcpyDeviceToHost));
+    CHECK(cudaMemcpy(arc_table, d_arc_table, arc_bytes, cudaMemcpyDeviceToHost));
 
     // free device global memory
-    //CHECK(cudaFree(d_mag_table));
-    //CHECK(cudaFree(d_arc_table));
+    CHECK(cudaFree(d_mag_table));
+    CHECK(cudaFree(d_arc_table));
 
     // reset device
     CHECK(cudaDeviceReset());
-
-// ----------------------- CUDA START -----------------------
-
-    // // set up data size of matrix
-    // int nx = 1 << 14;
-    // int ny = 1 << 14;
-    //
-    // int nxy = nx * ny;
-    // int nBytes = nxy * sizeof(float);
-    // printf("Matrix size: nx %d ny %d\n", nx, ny);
-    //
-    // // malloc host memory
-    // float *h_A, *h_B, *hostRef, *gpuRef;
-    // h_A = (float *)malloc(nBytes);
-    // h_B = (float *)malloc(nBytes);
-    // hostRef = (float *)malloc(nBytes);
-    // gpuRef = (float *)malloc(nBytes);
-    //
-    // // initialize data at host side
-    // double iStart = seconds();
-    // initialData(h_A, nxy);
-    // initialData(h_B, nxy);
-    // double iElaps = seconds() - iStart;
-    // printf("Matrix initialization elapsed %f sec\n", iElaps);
-    //
-    // memset(hostRef, 0, nBytes);
-    // memset(gpuRef, 0, nBytes);
-    //
-    // // malloc device global memory
-    // float *d_MatA, *d_MatB, *d_MatC;
-    // CHECK(cudaMalloc((void **)&d_MatA, nBytes));
-    // CHECK(cudaMalloc((void **)&d_MatB, nBytes));
-    // CHECK(cudaMalloc((void **)&d_MatC, nBytes));
-    //
-    // // transfer data from host to device
-    // CHECK(cudaMemcpy(d_MatA, h_A, nBytes, cudaMemcpyHostToDevice));
-    // CHECK(cudaMemcpy(d_MatB, h_B, nBytes, cudaMemcpyHostToDevice));
-    //
-    // // invoke kernel at host side
-    // int dimx = 32;
-    // dim3 block(dimx, 1);
-    // dim3 grid((nx + block.x - 1) / block.x, ny);
-    //
-    // iStart = seconds();
-    // sumMatrixOnGPUMix<<<grid, block>>>(d_MatA, d_MatB, d_MatC, nx, ny);
-    // CHECK(cudaDeviceSynchronize());
-    // iElaps = seconds() - iStart;
-    // printf("sumMatrixOnGPU2D <<<(%d,%d), (%d,%d)>>> elapsed %f sec\n", grid.x, grid.y, block.x, block.y, iElaps);
-    // // check kernel error
-    // CHECK(cudaGetLastError());
-    //
-    // // copy kernel result back to host side
-    // CHECK(cudaMemcpy(gpuRef, d_MatC, nBytes, cudaMemcpyDeviceToHost));
-    //
-    // // check device results
-    // //checkResult(hostRef, gpuRef, nxy);
-    //
-    // // free device global memory
-    // CHECK(cudaFree(d_MatA));
-    // CHECK(cudaFree(d_MatB));
-    // CHECK(cudaFree(d_MatC));
-    //
-    // // free host memory
-    // free(h_A);
-    // free(h_B);
-    // free(hostRef);
-    // free(gpuRef);
-    //
-    // // reset device
-    // CHECK(cudaDeviceReset());
-
-// ------------------------ CUDA END ------------------------
-
-
-
-
-
-
-
-
-
-
-
 
     free(mag_table);
     free(arc_table);
